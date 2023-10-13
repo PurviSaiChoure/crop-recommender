dfe508353b73f79c60837a6ee707950626aa8161
Rachita Dashore <rachita17dashore@gmail.com>
2023-10-13 18:14:26 +0530

    fix request.py

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'N':60, 'P':50, 'K':40,'temperature':20, 'humidity':80,'ph':7,'rainfall':200})
+r = requests.post(url,json={'nitrogen':60, 'phorsporus':50, 'potassium':40,'temperature':20, 'humidity':80,'pH':7,'rainfall':200})
 
 print(r.json())
