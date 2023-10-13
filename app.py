3a8d98aadb9ad1cae1e34eee042adc8c2106a061
Rachita Dashore <rachita17dashore@gmail.com>
2023-10-13 16:23:17 +0530

    ui

--- app.py
+++ app.py
@@ -4,29 +4,49 @@ from flask import Flask, request, jsonify, render_template
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
-model = joblib.load(open(r'C:\Users\purvi\Downloads\Crop-Recommender\app\final_model.joblib', 'rb'))
-sc = joblib.load(open(r'C:\Users\purvi\Downloads\Crop-Recommender\app\final_scaler.joblib','rb'))
+model = None
+scaler = None
+
+try:
+    model = joblib.load('final_model.joblib')
+    scaler = joblib.load('final_scaler.joblib')
+except Exception as e:
+    print("Error loading model or scaler:", str(e))
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    return render_template('index2.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     if request.method == "POST":
-        features = [float(x) for x in request.form.values()]  
-        input_data = np.array(features).reshape(1, -1)  
-        scaled_features = sc.transform(input_data)
-        prediction = model.predict(scaled_features)  
-        output = prediction[0]  
-
-        return render_template('result.html', prediction_text='The Recommended Crop to grow is {}'.format(output))
-
+        try:
+            features = [float(x) for x in request.form.values()]
+            input_data = np.array(features).reshape(1, -1)
+            if scaler is not None:
+                scaled_features = scaler.transform(input_data)
+                prediction = model.predict(scaled_features)
+                output = prediction[0]
+                return render_template('result2.html', prediction_text=output)
+            else:
+                return render_template('error.html', error_message='Scaler not loaded.')
+        except Exception as e:
+            return render_template('error.html', error_message=str(e))
     else:
-        return render_template('index.html')
+        return render_template('index2.html')
 
-    
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    try:
+        data = request.get_json(force=True)
+        if model is not None and scaler is not None:
+            prediction = model.predict([np.array(list(data.values()))])
+            output = prediction[0]
+            return jsonify({'prediction': output})
+        else:
+            return jsonify({'error': 'Model or scaler not loaded.'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
 if __name__ == "__main__":
     app.run(debug=True)
-
- 
